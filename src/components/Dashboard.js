0e92e72ba45516d8a3d05bd1febd65bf625889d6
ChrisPag <chrisp99@my.yorku.ca>
2022-01-13 03:59:25 -0500

    Organized files, updated styles

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,41 @@
-import NasaPic from "./NasaPic";
+import useFetch from './useFetch';
+import useDate from './useDate';
+import {FcLike} from 'react-icons/fc';
 
 const Dashboard = () => {
-    return (
-      <div className="dashboard">
-        <NasaPic />
-      </div>
-    );
+  //Get today's date
+  const endDate = useDate(0);
+  const startDate = useDate(2);
+ 
+  //Fetch image data
+  const {data: posts} = useFetch('https://api.nasa.gov/planetary/apod?api_key=W7iawCNJkLDutGN32iRFwseMCxmvT8LnYwG32XHM&start_date='
+  + startDate + '&end_date=' + endDate
+  );
+  
+  //Check if posts exist
+  if(posts){
+    //Create unique id for each object 
+    for(let i = 0; i < posts.length; i++){
+      posts[i]["id"] = i;
+    }
   }
+
+  return (
+    <div className="dashboard">
+      {posts && (posts.map((post)=>(
+        <div className="content" key={post.id}>
+          <img
+            src={post.url}
+            alt={post.title}
+          ></img>
+
+          <p><span>{post.title}</span> - {post.date}</p>
+          <button>Like <FcLike id="like"/></button>
+
+        </div>
+      )))}
+    </div>
+  );
+}
   
   export default Dashboard;
