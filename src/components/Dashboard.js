d90d15fcf6476a91c9a378fe2076c43602ff86e2
ChrisPag <chrisp99@my.yorku.ca>
2022-01-17 01:47:46 -0500

    Updated like button

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,28 +1,48 @@
 import useFetch from './useFetch';
 import useDate from './useDate';
-import LikeButton from './LikeButton';
+import { useState, useEffect } from 'react';
+//import LikeButton from './LikeButton';
 
 const Dashboard = () => {
   //Get today's date
   const endDate = useDate(0);
   const startDate = useDate(2);
-  //var likeText= "Like";
- 
+  let text = "Like";
+
   //Fetch image data
-  const {data: posts, numObjects} = useFetch('https://api.nasa.gov/planetary/apod?api_key=W7iawCNJkLDutGN32iRFwseMCxmvT8LnYwG32XHM&start_date='
+  const {data: posts, numObjects, isLoaded} = useFetch('https://api.nasa.gov/planetary/apod?api_key=W7iawCNJkLDutGN32iRFwseMCxmvT8LnYwG32XHM&start_date='
   + startDate + '&end_date=' + endDate
   );
-  
+
+  //const likedArray = new Array(numObjects).fill({text: 'Like'});
+  const [likedPosts, setLikedPosts] = useState(['Like', 'Like', 'Like']);
+
   //Check if posts exist
   if(posts){
-    //Create unique id for each object 
+    //Create unique id for each object, add like state
     for(let i = 0; i < posts.length; i++){
       posts[i]["id"] = i;
     }
   }
 
+  //update like state
+  const handleClick = (index) =>{
+    let newArr = [...likedPosts];
+
+    //newArr[index] = 'Unlike';
+    if(newArr[index]==='Like'){
+      newArr[index] = 'Unlike';
+    }
+    else{
+      newArr[index] = 'Like';
+    }
+    setLikedPosts(newArr);
+    console.log(likedPosts);
+  }
+
   return (
     <div className="dashboard">
+      {!isLoaded && <div>Loading...</div>}
       {posts && (posts.map(post=>(
         <div className="content" key={post.id}>
           <img
@@ -31,9 +51,8 @@ const Dashboard = () => {
           ></img>
 
           <p><span>{post.title}</span> - {post.date}</p>
-          {/*<button onClick={() => handleClick(post.id, post.liked)}>
-            {likeText} <FcLike id="like"/></button>*/}
-            <LikeButton postID ={post.id} numObjects = {numObjects}/>
+          <button onClick={()=>handleClick(post.id)}>{likedPosts[post.id]}</button>
+          {/*<LikeButton postID ={post.id} numObjects = {numObjects}/>*/}
         </div>
       )))}
     </div>
--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -1,18 +1,28 @@
 import {FcLike} from 'react-icons/fc';
+import { useState, useEffect } from 'react';
 
 const LikeButton = (postID, numObjects) => {
-
-    let likedPost = new Array(numObjects).fill(false);
-    let likeText = "Like";
-
-    const handleClick = (postID) =>{
-        likedPost[postID] = !likedPost[postID];
-        console.log(likedPost[postID]);
-    }
+    let text = "Like";
+ 
+    const [likedPosts, setLikedPosts] = useState(['Like', 'Like', 'Like']);
+    
+    const handleClick = (index) =>{
+        let newArr = [...likedPosts];
+    
+        if(newArr[index]==='Like'){
+          newArr[index] = 'Unlike';
+        }
+        else{
+          newArr[index] = 'Like';
+        }
+        setLikedPosts(newArr);
+        console.log(likedPosts);
+    
+      }
 
     return (
         <div className="likeButton">
-            <button onClick={() =>handleClick(postID)}>{/*likeText*/}Like <FcLike id="like"/></button>
+            <button onClick={()=>handleClick(postID)}>{likedPosts[postID]}<FcLike/></button>
         </div>
     );
 }
